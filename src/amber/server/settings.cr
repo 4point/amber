edff5b69d19d48f08eb9908d27bbf95709d2fb79
Isaac Sloan <isaac@isaacsloan.com>
2017-11-17 22:11:36 -0700

    settings refactor

--- src/amber/server/settings.cr
+++ src/amber/server/settings.cr
@@ -5,6 +5,10 @@ require "./ssl"
 module Amber
   class Settings
     include Amber::DSL::Server
+    alias WebSocketAdapter = WebSockets::Adapters::RedisAdapter.class | WebSockets::Adapters::MemoryAdapter.class
+    getter handler = Pipe::Pipeline.new
+    getter router = Router::Router.new
+    property pubsub_adapter : WebSocketAdapter = WebSockets::Adapters::MemoryAdapter
     property port_reuse : Bool
     property log : ::Logger
     property color : Bool = true
@@ -17,9 +21,9 @@ module Amber
     property ssl_cert_file : String? = nil
     property redis_url = ""
     property session : Hash(Symbol, Symbol | String | Int32)
-
-    # Loads environment yml settings from the current AMBER_ENV environment variable
-    # and defaults to development environment
-    {{ run("../scripts/environment_loader.cr") }}
   end
 end
+
+# Loads environment yml settings from the current AMBER_ENV environment variable
+# and defaults to development environment
+{{ run("../scripts/environment_loader.cr") }}
